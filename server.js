2efdd3f74eca7125ff55654965215b26c3951c8f
Aliasgergadi <1815.aliasgergadi@saifiyah.com>
2025-06-13 09:14:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
 app.post('/send-otp', async (req, res) => {
-  const { email } = req.body;
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  const { email } = req.body
+  const otp = Math.floor(100000 + Math.random() * 900000).toString()
 
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: '1815.aliasgergadi@saifiyah.com',
+        pass: 'qbrk sjin rvou emen'
       }
-    });
+    })
 
     await transporter.sendMail({
-      from: `Guess The Name <${process.env.EMAIL_USER}>`,
+      from: 'Guess The Name 1815.aliasgergadi@saifiyah.com',
       to: email,
       subject: 'Your OTP',
-      text: `Your OTP is: ${otp}`
-    });
+      text: Your OTP is: ${otp}
+    })
 
-    res.json({ success: true, otp });
+    res.json({ success: true, otp })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: "Email failed" });
+    console.error(error)
+    res.status(500).json({ success: false, error: "Email failed" })
   }
-});
+})
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+  console.log('Server running on port 3000')
+})
+
